ca5acf2eb5e8c432312a2ae5e488d6c069559032
Tatan <siurruti@gmail.com>
2025-08-01 20:03:13 -0400

    Arregla error en RevenueChart y actualiza dashboard

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,17 +18,8 @@ const sql = postgres(process.env.POSTGRES_URL!, {
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data;
+    const data = await sql`SELECT * FROM revenue`;
+    return data; // ✅ Asegúrate de retornar .rows
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
